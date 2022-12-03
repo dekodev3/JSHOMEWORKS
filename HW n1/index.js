af76ad19a8e6d58273ff559b769672e15dc1e7e5
dekodev3 <110020019+dekodev3@users.noreply.github.com>
2022-12-03 19:58:17 +0100

    Add files via upload

---
+++ HW n1/index.js
@@ -0,0 +1,55 @@
+// FIRST ASSIGMENT
+
+var firstName = "Dejan";
+    console.log("First Name");
+    console.log(firstName);
+    console.log(typeof(firstName));
+
+var secondName = null;
+    console.log("Second Name");
+    console.log(secondName);
+    console.log(typeof(secondNAme));
+
+var studentAge = 31;
+    console.log("Student Age");
+    console.log(studentAge);
+    console.log(typeof(studentAge));
+
+    var sedcStudent = true;
+    console.log("Is Dejan student of SEDC");
+    console.log(sedcStudent);
+    console.log(typeof(sedcStudent));
+
+    var favoriteSubject = "JavaScript";
+    console.log("Favorite Subject");
+    console.log(favoriteSubject);
+    console.log(typeof(favoriteSubject));
+
+    var favoriteTeacher;
+    console.log("Favorite Teacher");
+    console.log(favoriteTeacher);
+    console.log(typeof(favoriteTeacher));
+
+    
+    // SECOND ASSIGMENT
+
+    var pi = 3.14159265359;
+    var radius = 10;
+    var areaCircle = pi * (radius * 2);
+    console.log(areaCircle);
+    
+    // THIRD ASSIGMENT
+ 
+    var phonePrice = 119.95;
+    var taxRate  = 5;
+    var phones = 30;
+    var taxAmount = (taxRate / 100) * 119.95;
+    var totalPrice = phonePrice + taxAmount;
+    var priceTotalPhones = totalPrice * 30;
+    console.log("========Price for 1 PHONE======");
+    console.log(totalPrice);
+    console.log("========Price for 30 PHONE======");
+    console.log(priceTotalPhones);
+    console.log("========tax amount=======");
+    console.log(taxAmount);
+

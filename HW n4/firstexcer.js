25e03597c3cc743022855b3ca9f1834f6af4a596
dekodev3 <110020019+dekodev3@users.noreply.github.com>
2022-12-19 17:17:33 +0100

    Add files via upload

--- HW n4/firstexcer.js
+++ HW n4/firstexcer.js
@@ -1,10 +1,12 @@
+//First Excercise
+
 let userName = prompt("Write your Name");
 let mood = prompt("What's your mood today?");
 let activity = prompt("What activity are you doing today?");
 let userInputs = [userName, mood, activity];
 
-function myStory(userinputs){
-    message = `This is ${userName}. ${userName} is a nice person.Today they are ${mood}. They are ${activity} all day.`
+function myStory(userInputs){
+    let message = `This is ${userInputs[0]}. ${userInputs[0]} is a nice person.Today they are ${userInputs[1]}. They are ${userInputs[2]} all day.`
     alert(message);
     return message;
 }
